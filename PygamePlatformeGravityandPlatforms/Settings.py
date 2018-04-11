88c570d3bf1cfa79db1d4fde38c61e4e62d6a1bf
mgcg216 <36535459+mgcg216@users.noreply.github.com>
2018-04-11 15:51:20 -0700

    Add files via upload

---
+++ PygamePlatformeGravityandPlatforms/Settings.py
@@ -0,0 +1,17 @@
+# game options/settings
+TITLE = "Jump!"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+#Player Propperties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.06
+
+#Define colors
+WHITE = ( 255, 255, 255 )
+BLACK = (0, 0 , 0)
+RED = (255, 0 , 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
